8a43c127e904fd6700e4d7b09b1ba29c1f9bf055
Vladasprogrammer <voroninvlad3@gmail.com>
2025-01-31 15:39:51 +0200

    Lygtais supratau kaip veikia componentai, ir kaip juos reikia perteikti i kita faila su importu. Reikia daugiau praktikos.

---
+++ src/Components/ND/Props3.jsx
@@ -0,0 +1,8 @@
+export default function Props3({t1, t2, spalva}) {
+    return (
+        <>
+        <h1 style={{ color: spalva }}>{t1}</h1>
+        <h2 style={{ color: spalva }}>{t2}</h2>
+        </>
+    )
+}
