45a3e87b71c3242c0429f31808ceff9c22e90049
Vladasprogrammer <voroninvlad3@gmail.com>
2025-02-03 14:00:29 +0200

    Kaip ir done *shrug*

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './app.css';
 import './button.scss';
-import Labas from './Components/ND/Labas';
-import Props from './Components/ND/Props';
-import ZB from './Components/ND/ZB';
-import Props2 from './Components/ND/Props2';
-import Props3 from './Components/ND/Props3';
+import AK from "./Components/ND/AK";
+import CRandom from './Components/ND/CRandom';
+import MinusPlius from './Components/ND/MinusPlus';
+import Kvadratai from './Components/ND/Kvadratai';
+import RedBlue from './Components/ND/RedBlue';
+
 
 export default function App() {
 
@@ -12,12 +13,12 @@ export default function App() {
         <div className="App">
             <header className="App-header">
 
-                <Labas />
-                <Props t="Parašiau bet ką."/>
-                <ZB spalva="1"/>
-                <Props2 t1="Myliu Mamą" t2="Myliu Tėtį"/>
-                <Props3 t1="Saulė" t2="Mėnulis" spalva="orange"/>
-                
+                <AK />
+                <CRandom />
+                <MinusPlius />
+                <Kvadratai />
+                <RedBlue />
+
             </header>
         </div>
     )
