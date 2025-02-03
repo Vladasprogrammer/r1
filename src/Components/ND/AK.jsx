0fc17f09b13e4e8934b5977787ba904cf0d70be4
Vladasprogrammer <voroninvlad3@gmail.com>
2025-02-03 09:16:20 +0200

    Pirma uzduotis su react state padaryta.

---
+++ src/Components/ND/AK.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+
+export default function AK() {
+
+    const [isCircle, setIsCicle] = useState(true);
+
+    const shape = _ => {
+        setIsCicle(prevState => !prevState);
+    };
+    
+    const shapeStyle = {
+        width: '100px',
+        height: '100px',
+        backgroundColor: 'magenta',
+        marginBottom: '20px',
+        borderRadius: isCircle ? '50%' : '0%',
+    };
+
+    return (
+        <>
+        <div style={shapeStyle}></div>
+        <button onClick={shape}>Change</button>
+        </>
+    )
+}
