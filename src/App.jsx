511bd9bd762e7f32ed57e5e12e25b76e9d67e503
Vladasprogrammer <voroninvlad3@gmail.com>
2025-02-05 14:22:56 +0200

    destytojo failo parasytas appas.

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,28 @@ import './button.scss';
 import rand from './Functions/rand';
 import Sq from './Components/046/Sq';
 import { useState } from 'react';
-
+ 
 export default function App() {
  
     const [sq, setSq] = useState([]);
-
+ 
     const add = _ => {
-        setSq(s => [...s, rand()]);
+        setSq(s => [...s, rand(1000, 9999)]);
     }
-
+ 
     return (
         <div className="App">
             <header className="App-header">
-                
-                <div className='sq-bin'>
+ 
+                <div className="sq-bin">
                     {
                         sq.map((n, i) => <Sq key={i} num={n} />)
                     }
                 </div>
-                <button className='yellow' onClick={add}>Add</button>
+                <button className="yellow" onClick={add}>Add</button>
+ 
             </header>
         </div>
     );
 }
+ 
