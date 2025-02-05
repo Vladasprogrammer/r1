8948b4aabc4a91f31730244929dfbfc6682af296
Vladasprogrammer <voroninvlad3@gmail.com>
2025-02-05 17:57:59 +0200

    Uhhh... baigiau 46 paskaita.

--- src/Components/046/Sq.jsx
+++ src/Components/046/Sq.jsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+
 export default function Sq({ num }) {
+
+    useEffect(_ => {
+        console.log('Naujas kvadratukas');
+        return _ => {
+            console.log('Kvadratukas sunaikintas');
+        }
+    }, []);
+    
     return (
         <div className="sq">
             {num}
